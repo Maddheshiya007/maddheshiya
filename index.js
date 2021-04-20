bbe9d901182250f6285b6d7a9cd371716ed13b55
CHANDAN MADDHESHIYA <78289518+Maddheshiya007@users.noreply.github.com>
2021-04-20 23:49:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+// document.write("this is document dot write")
+// console.warn("this is waring")
+// console.error("this is an error")
+var nam1=60;
+var nam2=63;
+console.log(nam1+nam2);
+
+
+
