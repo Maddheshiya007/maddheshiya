1d11c4c856715a81f041a692c90be1b2a02ce067
CHANDAN MADDHESHIYA <78289518+Maddheshiya007@users.noreply.github.com>
2021-11-30 22:08:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-console.log("Hello World");
-// document.write("this is document dot write")
-// console.warn("this is waring")
-// console.error("this is an error")
-var nam1=60;
-var nam2=63;
-console.log(nam1+nam2);
+console.log("Hello World")
 
 
 
